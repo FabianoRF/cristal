b3cd5919f422a6b6a85f464484169f56b23dd623
FabianoRF <freis884@gmail.com>
2020-11-25 13:56:25 -0300

    Projeto criado, migrations iniciais criadas

---
+++ backend/src/shared/infra/typeorm/migrations/1606322587280-CreateProduct.ts
@@ -0,0 +1,61 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateProduct1606322587280 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'products',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'category',
+            type: 'varchar',
+          },
+          {
+            name: 'description',
+            type: 'text',
+          },
+          {
+            name: 'price',
+            type: 'decimal',
+            precision: 10,
+            scale: 2,
+          },
+          {
+            name: 'quantity',
+            type: 'int',
+          },
+          {
+            name: 'max_parcels',
+            type: 'int',
+          },
+
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('products');
+  }
+}
